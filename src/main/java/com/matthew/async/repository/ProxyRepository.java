07a32d67856aa1e4645bbbb553985f086957c65b
Matthew Franglen <matthew@franglen.org>
2016-08-12 00:07:02 +0100

    Start copying request details over to the proxied request

--- src/main/java/com/matthew/async/repository/ProxyRepository.java
+++ src/main/java/com/matthew/async/repository/ProxyRepository.java
@@ -1,7 +1,9 @@
 package com.matthew.async.repository;
 
 import java.net.URI;
+import java.util.Enumeration;
 import java.util.concurrent.CompletionStage;
+import java.util.function.BiFunction;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.rx.Rx;
+import org.glassfish.jersey.client.rx.RxInvocationBuilder;
 import org.glassfish.jersey.client.rx.RxWebTarget;
 import org.glassfish.jersey.client.rx.java8.RxCompletionStageInvoker;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,7 +36,31 @@ public class ProxyRepository {
     }
 
     public CompletionStage<Response> get(HttpServletRequest request) {
-        return target.request().rx().get();
+        RxInvocationBuilder<RxCompletionStageInvoker> builder = target.path(request.getRequestURI()).request();
+        builder = setHeaders(request, builder);
+        return builder.rx().get();
+    }
+
+    private RxInvocationBuilder<RxCompletionStageInvoker> setHeaders(HttpServletRequest request, RxInvocationBuilder<RxCompletionStageInvoker> builder) {
+        return reduce(request.getHeaderNames(), builder, applyHeader(request));
+    }
+
+    private BiFunction<RxInvocationBuilder<RxCompletionStageInvoker>, String, RxInvocationBuilder<RxCompletionStageInvoker>> applyHeader(HttpServletRequest request) {
+        return (RxInvocationBuilder<RxCompletionStageInvoker> builder, String header) -> reduce(request.getHeaders(header), builder, applyHeader(header));
+    }
+
+    private BiFunction<RxInvocationBuilder<RxCompletionStageInvoker>, String, RxInvocationBuilder<RxCompletionStageInvoker>> applyHeader(String header) {
+        return (RxInvocationBuilder<RxCompletionStageInvoker> builder, String value) -> builder.header(header, value);
+    }
+
+    private <T, V> T reduce(Enumeration<V> values, T initial, BiFunction<T, V, T> reducer) {
+        T current = initial;
+
+        while (values.hasMoreElements()) {
+            current = reducer.apply(current, values.nextElement());
+        }
+
+        return current;
     }
 
 }
