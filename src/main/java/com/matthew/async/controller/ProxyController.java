85775601b7a7ab2bdc8b103cb2b711f4b33964f5
Matthew Franglen <matthew@franglen.org>
2016-08-11 23:49:17 +0100

    Very simple working proxy

--- src/main/java/com/matthew/async/controller/ProxyController.java
+++ src/main/java/com/matthew/async/controller/ProxyController.java
@@ -1,18 +1,69 @@
 package com.matthew.async.controller;
 
-import java.util.concurrent.Future;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.matthew.async.repository.ProxyRepository;
 
 @Controller
 public class ProxyController {
 
-    @RequestMapping("/**")
-    public Future<String> handle(HttpServletRequest request) {
+    private static final Set<String> EXCLUDED_HEADERS = new HashSet<>(Arrays.asList("Transfer-Encoding"));
 
+    @Autowired private ProxyRepository repository;
+
+    @RequestMapping("/**")
+    public @ResponseBody CompletionStage<ResponseEntity<String>> handle(HttpServletRequest request) {
+        return repository.get(request)
+            .thenApply(this::convert);
+    }
+
+    private ResponseEntity<String> convert(Response jaxResponse) {
+        HttpHeaders headers = convert(jaxResponse.getHeaders(), EXCLUDED_HEADERS);
+        HttpStatus status = HttpStatus.valueOf(jaxResponse.getStatus());
+        String result = jaxResponse.readEntity(String.class);
+
+        return new ResponseEntity<String>(result, headers, status);
+    }
+
+    private HttpHeaders convert(MultivaluedMap<String, Object> headers, Set<String> excluded) {
+        HttpHeaders result = new HttpHeaders();
+        headers.entrySet().stream()
+            .filter(isExcluded(excluded))
+            .forEach(set(result));
+
+        return result;
+    }
+
+    private Predicate<Map.Entry<String, List<Object>>> isExcluded(Set<String> excluded) {
+        return (Map.Entry<String, List<Object>> entry) -> ! excluded.contains(entry.getKey());
+    }
+
+    private Consumer<Map.Entry<String, List<Object>>> set(HttpHeaders headers) {
+        return (Map.Entry<String, List<Object>> entry) -> {
+            String key = entry.getKey();
+            entry.getValue().stream()
+                .map(String::valueOf)
+                .forEach(value -> headers.add(key, value));
+        };
     }
 
 }
--- src/main/java/com/matthew/async/repository/ProxyRepository.java
+++ src/main/java/com/matthew/async/repository/ProxyRepository.java
@@ -1,16 +1,39 @@
 package com.matthew.async.repository;
 
-import java.util.concurrent.Future;
+import java.net.URI;
+import java.util.concurrent.CompletionStage;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
+import org.glassfish.jersey.client.rx.Rx;
+import org.glassfish.jersey.client.rx.RxWebTarget;
+import org.glassfish.jersey.client.rx.java8.RxCompletionStageInvoker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProxyRepository {
 
-    public Future<String> request(HttpServletRequest request) {
+    @Value("${proxy.host:localhost}")
+    private String host;
 
+    @Value("${proxy.port:80}")
+    private int port;
+
+    private RxWebTarget<RxCompletionStageInvoker> target;
+
+    @PostConstruct
+    public void init() {
+        URI url = UriBuilder.fromPath("http://{host}:{port}").build(host, port);
+        target = Rx.newClient(RxCompletionStageInvoker.class)
+            .target(url);
+    }
+
+    public CompletionStage<Response> get(HttpServletRequest request) {
+        return target.request().rx().get();
     }
 
 }
